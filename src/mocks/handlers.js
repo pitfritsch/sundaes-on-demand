532c0b3b3f2cf34937f5cd338b2932e0d7cc0466
Pedro Fritsch <pedrofritsch98@gmail.com>
2022-08-02 11:48:00 +1000

    testing and coding toppings

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,31 @@
 import { rest } from 'msw'
 
+const serverUrl = 'http://localhost:3030'
 
 export const handlers = [
-  rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+  rest.get(`${serverUrl}/scoops`, (req, res, ctx) => {
     return res(
       ctx.json([
         { name: 'Chocolate', imagePath: '/images/chocolate.png' },
         { name: 'Vanilla', imagePath: '/images/vanilla.png' },
+      ])
+    )
+  }),
+  rest.get(`${serverUrl}/toppings`, (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          name: "M&Ms",
+          imagePath: "/images/m-and-ms.png"
+        },
+        {
+          name: "Hot fudge",
+          imagePath: "/images/hot-fudge.png"
+        },
+        {
+          name: "Peanut butter cups",
+          imagePath: "/images/peanut-butter-cups.png"
+        }
       ])
     )
   })
--- src/pages/entry/test/Otions.test.js
+++ src/pages/entry/test/Otions.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import Options from "../Options"
 
-describe('Testing sundae options',  () => {
+describe('Testing scoop options',  () => {
   test('display images for each scoop from server', async () => {
     render(<Options optionType={'scoops'} />)
 
@@ -10,5 +10,17 @@ describe('Testing sundae options',  () => {
 
     const altTexts = scoopImages.map(image => image.alt)
     expect(altTexts).toEqual(['Chocolate scoop', 'Vanilla scoop'])
+  })
+})
+
+describe('Testing topping options',  () => {
+  test('display images for each topping option', async () => {
+    render(<Options optionType={'toppings'} />)
+
+    const toppingImages = await screen.findAllByRole('img', { name: /topping$/i })
+    expect(toppingImages).toHaveLength(3)
+
+    const texts = toppingImages.map(img => img.alt)
+    expect(texts).toEqual(['M&Ms topping', 'Hot fudge topping', 'Peanut butter cups topping'])
   })
 })
